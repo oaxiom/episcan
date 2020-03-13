66651d03d31447f95451fa1a6ab8f0e4704deba1
oaxiom <oaxiom@gmail.com>
2020-03-13 10:17:54 +0800

    heatmap of domain usage

--- 2.determine_reqd_models/3.model_matrix.py
+++ 2.determine_reqd_models/3.model_matrix.py
@@ -25,19 +25,27 @@ print('Found {0:,} unique domains'.format(len(doms)))
 tab = {}
 ensp_lookup = {}
 for ensp, name in zip(all_epifactors['ensp'], all_epifactors['name']):
-    tab[(ensp, name)] = {k: 0 for k in doms}    
+    tab[(ensp, name)] = {k: 0 for k in doms}
     ensp_lookup[ensp] = name
 
 for line in pfam:
     hmm = line['score'] # all in the wrong columns
     name = line['dom_acc']
     ensp = line['peptide']
-    
+
     tab[(ensp, ensp_lookup[ensp])]['{0}:{1}'.format(name, hmm)] = 1
-    
+
 oh = open('model_matrix_hits.tsv', 'w')
 oh.write('{0}\n'.format('\t'.join(['ensp', 'name'] + doms)))
+gltab = []
 for item in tab:
     row = [tab[item][d] for d in doms] # enforce order just in case
     oh.write('{0}\n'.format('\t'.join(list(item) + [str(i) for i in row])))
+    gltab.append({'name': item[1], 'ensp': item[0], 'conditions': [i for i in row]})
 oh.close()
+
+
+
+# heatmap
+e = expression(loadable_list=gltab, cond_names=doms)
+e.heatmap('all_models.pdf', border=True)
