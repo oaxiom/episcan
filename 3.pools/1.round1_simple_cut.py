c2961ffe32d9dc83fdbf15811a562b93b61bdafe
oaxiom <oaxiom@gmail.com>
2020-03-13 15:27:27 +0800

    filtered out more TFs from the initial list

--- 3.pools/1.round1_simple_cut.py
+++ 3.pools/1.round1_simple_cut.py
@@ -9,30 +9,7 @@ Basically, if they find the same proteins, we can trim them from the list
 
 '''
 
-pfam = genelist(filename='../2.determine_reqd_models/pfam.txt', format=format.hmmer_tbl)
-
 model_matrix = glload('../2.determine_reqd_models/domains.glb')
-
 all_models = model_matrix.getConditionNames()
 
-doms = {}
-
-for dom in pfam:
-    if dom['dom_acc'] not in doms:
-        doms[dom['dom_acc']] = []
-    doms[dom['dom_acc']].append(dom['peptide'])
-
-pair_doms = []
-for i1, dom1 in enumerate(doms):
-    for i2, dom2 in enumerate(doms):
-        if i1 > i2:
-            if len(dom1) == len(dom2):
-                # Check all the same
-                if False not in [i1 == i2 for i1, i2 in zip(doms[dom1], doms[dom2])]:
-                    pair_doms.append(dom2)
-                    pair_doms.append(dom1)
-                    # Work out the sum of scores...
-
-print(pair_doms)
-
-
+for
