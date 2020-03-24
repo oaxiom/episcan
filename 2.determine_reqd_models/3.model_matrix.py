b847138a619735aa483460b8121a41417ffcef7a
oaxiom <oaxiom@gmail.com>
2020-03-24 16:21:09 +0800

    round 1 domain set

--- 2.determine_reqd_models/3.model_matrix.py
+++ 2.determine_reqd_models/3.model_matrix.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-import sys, re
+import sys, re, os
 from glbase3 import *
 
 # Generates a matrix of genes (rows) and models, where a 0 and 1 is used to indicate
 # if the HMM is used or not used.
 
 all_epifactors = glload('../1.extract_epifactors_FASTA/hs_epifactors.all.glb')
-pfam = genelist(filename='pfam.txt', format=format.hmmer_tbl)
-
+pfam = genelist(filename='pfam.txt', format=format.hmmer_domtbl)
+print(pfam)
 # Get the list of domains in use
 doms = []
 for line in pfam:
-    hmm = line['score'] # all in the wrong columns
-    name = line['dom_acc']
+    hmm = line['dom_acc']
+    name = line['dom_name']
 
     if '{0}:{1}'.format(name, hmm) not in doms:
         doms.append('{0}:{1}'.format(name, hmm))
@@ -29,8 +29,8 @@ for ensp, name in zip(all_epifactors['ensp'], all_epifactors['name']):
     ensp_lookup[ensp] = name
 
 for line in pfam:
-    hmm = line['score'] # all in the wrong columns
-    name = line['dom_acc']
+    hmm = line['dom_acc']
+    name = line['dom_name']
     ensp = line['peptide']
 
     tab[(ensp, ensp_lookup[ensp])]['{0}:{1}'.format(name, hmm)] = 1
@@ -50,19 +50,25 @@ e.save('model_matrix.glb')
 
 filte = e.filter_low_expressed(0.9,1)
 detected = filte.removeDuplicates('name')
-undetected = detected.map(genelist=e, key='name', logic='notright').getColumns(['ensp', 'name'], strip_expn=True)
-undetected = undetected.removeDuplicates('name')
-undetected.sort('name')
-undetected.saveTSV('undetected_by_hmmer.tsv')
+print(detected.getColumns(['name'], strip_expn=True))
+print(e.getColumns(['name'], strip_expn=True))
+os.remove('undetected_by_hmmer.tsv')
+undetected = detected.map(genelist=e, key='name', logic='notright')
+if undetected:
+    undetected = undetected.getColumns(['ensp', 'name'], strip_expn=True)
+    undetected = undetected.removeDuplicates('name')
+    undetected.sort('name')
+    undetected.saveTSV('undetected_by_hmmer.tsv')
+
+    if len(undetected) < len(detected):
+        print('Warning! Undetected={0} < {1}=AllNames, reduce your Evalue?'.format(len(undetected), len(detected)))
+
 filte.getColumns(['ensp', 'name'], strip_expn=True).saveTSV('detected_by_hmmer.tsv')
 
 all_ensp = len(e)
 
-if len(undetected) < len(detected):
-    print('Warning! Undetected={0} < {1}=AllEnsp, reduce your Evalue?'.format(len(undetected), len(detected)))
-
 config.draw_mode = 'pdf'
 filte.heatmap('all_models.pdf', border=True, imshow=True,
     col_cluster=True,
-    heat_wid=0.75, optimal_ordering=False,
+    heat_wid=0.75, optimal_ordering=True,
     size=[12,12])
--- 3.pools/1.round1_simple_cut.py
+++ 3.pools/1.round1_simple_cut.py
@@ -1,15 +1,77 @@
 #!/usr/bin/env python3
+import numpy
 from glbase3 import *
 
 '''
 
 Round 1, we cut domains that are redundant with other doamins.
 
-Basically, if they find the same proteins, we can trim them from the list
+Basically, if they find the exact same proteins, we can trim them from the list
 
 '''
 
-model_matrix = glload('../2.determine_reqd_models/domains.glb')
+model_matrix = glload('../2.determine_reqd_models/model_matrix.glb')
 all_models = model_matrix.getConditionNames()
 
-for
+# Now I need to work out the most senstiive one, and keep that:
+pfam = genelist(filename='../2.determine_reqd_models/pfam.txt', format=format.hmmer_domtbl)
+print(pfam)
+scores = {}
+
+for model in all_models:
+    this_model = model_matrix.getDataForCondition(model)
+    s = sum(this_model)
+    if s not in scores:
+        scores[s] = []
+    scores[s].append((model, this_model))
+
+doms_to_keep = []
+
+# test those with a matching score
+todo = len(scores)
+for idx, score in enumerate(scores):
+    if len(scores[score]) == 1:
+        doms_to_keep.append({'name': scores[score][0][0], 'frequency': score})
+        continue
+
+    dupes = set([])
+    for i1, m1 in enumerate(scores[score]):
+        for i2, m2 in enumerate(scores[score]):
+            if i1 < i2:
+                if numpy.array_equal(m1[1], m2[1]):
+                    dupes.add(m1[0])
+                    dupes.add(m2[0])
+
+    if dupes:
+        dupes = set(dupes)
+        #print('{0} are dupes'.format('\t'.join(list(dupes))))
+
+        dupe_es = {d: 0.0 for d in dupes}
+        # get the sum of e-values for each HMM:
+        for i in pfam:
+            hmm_name = '{0}:{1}'.format(i['dom_name'], i['dom_acc'])
+            if hmm_name in dupe_es:
+                dupe_es[hmm_name] += float(i['e'])
+
+        best_e = min(dupe_es, key=dupe_es.get)
+        doms_to_keep.append({'name': best_e, 'frequency': score})
+
+    else: # need to add all the PWMs:
+        for m in scores[score]:
+            doms_to_keep.append({'name': m[0], 'frequency': score})
+
+    if idx % 10 == 0:
+        print('Done {0}/{1}'.format(idx, todo))
+
+print(doms_to_keep)
+
+# This will be the initial seed for the pools;
+gl = genelist()
+gl.load_list(doms_to_keep)
+gl.sort('frequency')
+gl.reverse()
+gl.save('domains_round1.glb')
+gl.saveTSV('domains_round1.txt', key_order=['name'])
+
+
+
