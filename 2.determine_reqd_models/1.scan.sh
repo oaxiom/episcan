b4bf232dc6f529d9a11f5f30a80633790c4159f2
oaxiom <oaxiom@gmail.com>
2020-04-30 07:03:08 +0800

    some additional filtering on epifactors

--- 2.determine_reqd_models/1.scan.sh
+++ 2.determine_reqd_models/1.scan.sh
@@ -1,11 +1,5 @@
-#PBS -l nodes=1:ppn=2
-#PBS -q batch
-#PBS -j oe
-#PBS -o results.out
-#PBS -V 
-cd $PBS_O_WORKDIR
 
-hmmsearch -E 1e-2 --cpu 2 --noali --domtblout pfam.txt ../domains/domains.hmm ../1.extract_epifactors_FASTA/hs_epifactors.all.fa >/dev/null
+hmmsearch -E 1e-2 --cpu 4 --noali --domtblout pfam.txt ../domains/domains.hmm ../1.extract_epifactors_FASTA/hs_epifactors.all.fa >/dev/null
 
 
 
