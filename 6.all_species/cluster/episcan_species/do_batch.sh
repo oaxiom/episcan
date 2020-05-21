8716c9b88eb8eb831ddfbc2790c05b37b92376ac
oaxiom <oaxiom@gmail.com>
2020-05-22 07:58:07 +0800

    all species search update

--- 6.all_species/cluster/episcan_species/do_batch.sh
+++ 6.all_species/cluster/episcan_species/do_batch.sh
@@ -9,8 +9,19 @@ do
     echo Species = $base
     qsub -N hmm.$base -v inp=$species,out=$base hmm_job.pbs 
     echo
-    sleep 1
+    sleep 0.1
 done
 
+
+for species in /share/apps/genomics/genome/ensembl_genomes/pep/*/*/pep/*.fa.gz
+do
+    root=`basename $species`
+    base=`echo $root | sed -r 's/.pep.all.fa.gz//g'`
+
+    echo Species = $base
+    qsub -N hmm.$base -v inp=$species,out=$base hmm_job.pbs
+    echo
+    sleep 0.1
+done
 
 
